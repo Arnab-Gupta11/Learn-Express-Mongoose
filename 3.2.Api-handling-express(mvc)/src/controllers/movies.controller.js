ba35d502b69b86ed36b30deb952064195fb777c9
Arnab Gupta <arnabgupta1111@gmail.com>
2024-05-05 14:17:04 +0600

    3.2.Api-handling-express(mvc): Use param middleware

--- 3.2.Api-handling-express(mvc)/src/controllers/movies.controller.js
+++ 3.2.Api-handling-express(mvc)/src/controllers/movies.controller.js
@@ -1,13 +1,23 @@
-// const express = require("express");
 const fs = require("fs");
-
-// fs.readFileSync return data in Stringfy formate. So convert to object we need to parse it.
-
 const movies = JSON.parse(fs.readFileSync("./public/data/movies.json", "utf-8"));
 
-//--------> Route handler function <---------
-//route handler function also kind of middleware.But they called on some specific route.
-//req and res parameter are objects.these object go through all the middlewqre to route handler function.
+// <-------- Param Middleware(see notions note) --------->
+//In this middleware, there is a value parameter, it will store the value of specific params when called.you can take any name for value.
+const checkId = async (req, res, next, value) => {
+  const movie = movies.find((el) => {
+    return el.id === value * 1;
+  });
+
+  //If movie is not found
+  if (!movie) {
+    return res.status(404).json({
+      status: "fail",
+      message: `Movie with id: ${value} is not found`,
+    });
+  }
+  req.requsetedMovie = movie;
+  next();
+};
 
 const getAllMovies = async (req, res) => {
   //response send in JSend JSON formate.
@@ -39,21 +49,7 @@ const createNewMovie = async (req, res) => {
   });
 };
 const getMovie = async (req, res) => {
-  const { id } = req.params;
-
-  //Using javascript find method searching movie by id.
-  const movie = movies.find((el) => {
-    return el.id === id * 1; //converting id value string to integer.
-  });
-
-  //If movie is not found
-  if (!movie) {
-    return res.status(404).json({
-      status: "fail",
-      message: `Movie with id: ${id} is not found`,
-    });
-  }
-
+  const movie = req.requsetedMovie;
   //If movie found send the movie as response.
   res.status(200).json({
     status: "success",
@@ -63,24 +59,10 @@ const getMovie = async (req, res) => {
   });
 };
 const updateMovie = async (req, res) => {
-  const id = req.params.id;
-  //Serching the movie using id
-  const movieToUpdate = movies.find((el) => {
-    return el.id === id * 1;
-  });
-  if (!movieToUpdate) {
-    return res.status(404).json({
-      status: "fail",
-      message: `Movie with id: ${id} is not found`,
-    });
-  }
+  const movieToUpdate = req.requsetedMovie;
   const updatedMovieIndex = movies.indexOf(movieToUpdate);
-  //When two object have different property then assign method combine both object and return a new object.
-  //When two object have some property same then 2nd object value is updated to 1st object.that means 1st object is updated with 2nd object value.we use this here
   Object.assign(movieToUpdate, req.body);
-
   movies[updatedMovieIndex] = movieToUpdate;
-
   fs.writeFile("./public/data/movies.json", JSON.stringify(movies), (err) => {
     res.status(200).json({
       status: "success",
@@ -91,17 +73,7 @@ const updateMovie = async (req, res) => {
   });
 };
 const deleteMovie = async (req, res) => {
-  const id = req.params.id;
-  //Serching the movie using id
-  const movieToDelete = movies.find((el) => {
-    return el.id === id * 1;
-  });
-  if (!movieToDelete) {
-    return res.status(404).json({
-      status: "fail",
-      message: `Movie with id: ${id} is not found for delete`,
-    });
-  }
+  const movieToDelete = req.requsetedMovie;
   const deletedMovieIndex = movies.indexOf(movieToDelete);
   movies.splice(deletedMovieIndex, 1);
   fs.writeFile("./public/data/movies.json", JSON.stringify(movies), (err) => {
@@ -120,4 +92,5 @@ module.exports = {
   createNewMovie,
   updateMovie,
   deleteMovie,
+  checkId,
 };
--- 3.2.Api-handling-express(mvc)/src/routes/movies.routes.js
+++ 3.2.Api-handling-express(mvc)/src/routes/movies.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
-const { getAllMovies, getMovie, createNewMovie, updateMovie, deleteMovie } = require("../controllers/movies.controller");
+const { getAllMovies, getMovie, createNewMovie, updateMovie, deleteMovie, checkId } = require("../controllers/movies.controller");
 
 const router = express.Router();
+
+//param middleware
+router.param("id", checkId);
 
 router.route("/").get(getAllMovies).post(createNewMovie);
 router.route("/:id").get(getMovie).patch(updateMovie).delete(deleteMovie);
