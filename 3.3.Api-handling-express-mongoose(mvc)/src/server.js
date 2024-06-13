876ef56510e41ed7d6275b65983f2b6b2ab70a95
Arnab Gupta <arnabgupta1111@gmail.com>
2024-06-13 21:57:40 +0600

    3.3.Api-handling-express-mongoose: Create a movie schema

--- 3.3.Api-handling-express-mongoose(mvc)/src/server.js
+++ 3.3.Api-handling-express-mongoose(mvc)/src/server.js
@@ -2,6 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const connectToDB = require("./db");
+const { default: mongoose } = require("mongoose");
 const port = process.env.PORT || 3000;
 
 //Connect server to database
@@ -15,6 +16,32 @@ connectToDB()
   .catch((err) => {
     console.log("MongoDB connection failed !!! ", err);
   });
+
+//Schema based model: we create model based on schema and using that model we can crete document, querying on that document.
+//1.Create Schema: like bluprint of your collection.
+const movieSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Movie name is required"],
+    uniqe: true,
+  },
+  description: {
+    type: String,
+  },
+  duration: {
+    type: Number,
+    required: [true, "Duration is required"],
+  },
+  rating: {
+    type: Number,
+    default: 1.0,
+  },
+});
+//2. Create a model based on this schema so that we can manipulate collection.
+// const model-name = mongoose.model("collection-name", schema-name);
+//model-nmae => By using this model name we manipulate the collection in our server.
+//collection-name: we write the collection-name as singular form, by default mongodb create a collection in plural name.
+const movie = mongoose.model("Movie", movieSchema);
 
 //when we start server by default node initialize many variable that we can use anywhere.that is environment variable.(see notion note)
 // console.log(process.env); //It will show all the variable set in process.env
